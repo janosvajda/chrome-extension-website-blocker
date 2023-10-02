972112ef756a8fdca7a1e319545ed187595a709c
Janos Vajda <vajdajanos@gmail.com>
2023-10-02 12:31:48 +0100

    Much smarter options page, and more sophisticated blocking mechanism.

--- background.js
+++ background.js
@@ -1,23 +1,47 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.get(["blocked", "enabled"],  (local) => {
+    chrome.storage.local.get(["blocked", "enabled"], (local) => {
         if (!Array.isArray(local.blocked)) {
-            chrome.storage.local.set({ blocked: [] });
+            chrome.storage.local.set({blocked: []});
         }
     });
 });
 
-chrome.tabs.onUpdated.addListener( (tabId, changeInfo) => {
-    const url = changeInfo.pendingUrl || changeInfo.url;
+// Initialize a dictionary to keep track of blocked tabs
+const blockedTabs = {};
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    const url = changeInfo.pendingUrl || tab.url;
     if (!url || !url.startsWith("http")) {
         return;
     }
     console.info('Opened URL', url);
-    const hostname = new URL(url).hostname;
-    chrome.storage.local.get(["blocked", "enabled"],  (local) => {
-        const { blocked } = local;
-        if (Array.isArray(blocked) && blocked.find(domain => hostname.includes(domain))) {
-            chrome.tabs.remove(tabId);
-            chrome.tabs.create({ url: "warning.html" });
+    const urlParts = new URL(url);
+    let hostname = urlParts.hostname.replace(/^www\./, '');
+
+    // Retrieve the list of blocked websites from local storage
+    chrome.storage.local.get("blocked", (local) => {
+        const blockedWebsites = local.blocked || [];
+
+        // Check if the block is enabled for the current hostname
+        const blockedWebsite = blockedWebsites.find((website) => website.name === hostname);
+
+        if (blockedWebsite && blockedWebsite.enabled) {
+            // Check if the tab is not already blocked
+            if (!blockedTabs[tabId]) {
+                // Mark the tab as blocked to prevent further actions on it
+                blockedTabs[tabId] = true;
+
+                // Remove the tab and create a new tab with the warning page
+                chrome.tabs.remove(tabId, () => {
+                    chrome.tabs.create({url: "warning.html"});
+                });
+            }
+        } else {
+            // If the tab is unblocked, clear it from the dictionary
+            delete blockedTabs[tabId];
         }
     });
 });
+
+
+
--- options.js
+++ options.js
@@ -1,17 +1,104 @@
-const textarea = document.getElementById("textarea");
-const save = document.getElementById("save");
-const checkbox = document.getElementById("checkbox");
+const websiteList = document.getElementById("websiteList");
+const addButton = document.getElementById("addButton");
+const newWebsiteInput = document.getElementById("newWebsite");
 
-save.addEventListener("click", () => {
-    const blocked = textarea.value.split("\n").map(s => s.trim()).filter(Boolean);
-    chrome.storage.local.set({ blocked });
-});
+// Function to create a new website item with a delete button
+function createWebsiteItem(website, enabled) {
+    const websiteItem = document.createElement("div");
+    websiteItem.className = "websiteItem";
 
-window.addEventListener("DOMContentLoaded", () => {
-    chrome.storage.local.get(["blocked", "enabled"], function (local) {
-        const { blocked } = local;
-        if (Array.isArray(blocked)) {
-            textarea.value = blocked.join("\n");
+    const websiteName = document.createElement("div");
+    websiteName.className = "websiteName";
+    websiteName.textContent = website;
+
+    const websiteCheckbox = document.createElement("input");
+    websiteCheckbox.type = "checkbox";
+    websiteCheckbox.className = "websiteCheckbox";
+    websiteCheckbox.checked = enabled;
+
+    // Add an event listener to the checkbox to update local storage when checked or unchecked
+    websiteCheckbox.addEventListener("change", () => {
+        saveToLocalStorage();
+    });
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => {
+        websiteItem.remove(); // Remove the website item from the UI
+        removeFromLocalStorage(website); // Remove the website from local storage
+    });
+
+    websiteItem.appendChild(websiteName);
+    websiteItem.appendChild(websiteCheckbox);
+    websiteItem.appendChild(deleteButton);
+    websiteList.appendChild(websiteItem);
+
+    return websiteItem;
+}
+
+// Function to remove a website from local storage
+function removeFromLocalStorage(websiteToRemove) {
+    chrome.storage.local.get({ blocked: [] }, (data) => {
+        const blockedWebsites = data.blocked;
+
+        // Filter out the website to remove
+        const updatedBlockedWebsites = blockedWebsites.filter((website) => {
+            return website.name !== websiteToRemove;
+        });
+
+        // Store the updated list in local storage
+        chrome.storage.local.set({ blocked: updatedBlockedWebsites });
+    });
+}
+
+// Function to load data from local storage and populate the list
+function loadAndPopulateWebsiteList() {
+    chrome.storage.local.get({ blocked: [] }, (data) => {
+        const blockedWebsites = data.blocked;
+
+        if (blockedWebsites && blockedWebsites.length > 0) {
+            blockedWebsites.forEach((website) => {
+                createWebsiteItem(website.name, website.enabled);
+            });
         }
     });
+}
+
+// Event listener to add a new website when Enter key is pressed
+newWebsiteInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        const websiteName = newWebsiteInput.value.trim();
+        if (websiteName) {
+            createWebsiteItem(websiteName, true);
+            newWebsiteInput.value = "";
+            saveToLocalStorage(); // Save the updated list to local storage
+        }
+    }
 });
+
+// Load and populate the website list when the page is loaded
+window.addEventListener("DOMContentLoaded", () => {
+    loadAndPopulateWebsiteList();
+});
+
+// Event listener to add a new website
+addButton.addEventListener("click", () => {
+    const websiteName = newWebsiteInput.value.trim();
+    if (websiteName) {
+        createWebsiteItem(websiteName, true);
+        newWebsiteInput.value = "";
+        saveToLocalStorage(); // Save the updated list to local storage
+    }
+});
+
+// Function to save the enabled/disabled status of websites to local storage
+function saveToLocalStorage() {
+    const websiteItems = document.querySelectorAll(".websiteItem");
+    const blocked = Array.from(websiteItems).map(item => ({
+        name: item.querySelector(".websiteName").textContent,
+        enabled: item.querySelector(".websiteCheckbox").checked
+    }));
+
+    // Store the blocked websites in chrome.storage.local
+    chrome.storage.local.set({ blocked });
+}
