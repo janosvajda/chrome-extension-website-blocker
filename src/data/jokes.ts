1ed9a442f5cf704a0a9277983076cbbda1f06729
Janos Vajda <vajdajanos@gmail.com>
2023-11-28 05:51:35 +0000

    Code refactoring + webpack has been added and configured.

---
+++ src/data/jokes.ts
@@ -0,0 +1,8 @@
+// Arrays of jokes
+export const jokes = [
+    "Why do programmers always mix up Christmas and Halloween? Because Oct 31 == Dec 25.",
+    "Why was the math book sad? Because it had too many problems.",
+    "I'm reading a book on anti-gravity. It's impossible to put down!",
+    "Why did the mathematician throw his clock out the window? He wanted to see time fly.",
+    "Why did the computer catch a cold? Because it had too many windows open.",
+];
