a8108da998a3ec957c08f4551e26036c5fb201a4
Janos Vajda <vajdajanos@gmail.com>
2023-11-29 17:55:23 +0000

    blockedMessages has been refactored + warning page has been redesigned.

--- src/content.ts
+++ src/content.ts
@@ -1,9 +1,18 @@
 import {blockedMessages} from "./helper/blockMessages";
 
-// Randomly select a blocked site message
 const randomIndex = Math.floor(Math.random() * blockedMessages.length);
-const message = blockedMessages[randomIndex];
+const { message, type, randomItem } = blockedMessages[randomIndex];
 
-// Display the message in the center of the screen
-const messageDiv = document.getElementById("message");
+console.log('JJJ', randomItem);
+
+const messageDiv = document.getElementById("message") as HTMLDivElement;
+const randomItemDiv = document.getElementById("randomItem") as HTMLDivElement;
+
+messageDiv.innerText = '';
+randomItemDiv.innerText = '';
+
 messageDiv.innerText = message;
+
+if (randomItem) {
+    randomItemDiv.innerText = randomItem;
+}
